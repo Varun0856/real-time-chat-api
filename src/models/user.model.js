c21c3041d25eae4491adafcd0ec07e84272da3f8
Varun0856 <Varun0856@users.noreply.github.com>
2025-10-10 21:35:08 +0530

    Update User Schema model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
+import validator from "validator";
 
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: [true, "Username is required"],
         trim: true,
-        minLength: 4,
-        maxLength: 20,
+        minlength: 4,
+        maxlength: 20,
         lowercase: true,
         unique: [true, "Username must be unique"]
     },
@@ -14,32 +15,44 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, "Firstname is required"],
         trim: true,
-        minLength: 2
+        minlength: 2
     },
     lastname: {
         type: String,
         required: [true, "Lastname is required"],
         trim: true,
-        minLength: 2,
+        minlength: 2,
     },
     email: {
         type: String,
         required: [true, "Email is required"],
         trim: true,
         lowercase: true,
-        unique: [true, "Email must be unique"],
-        match: [/\S+@\S+\.\S+/, 'Please fill a valid email']
+        unique: true,
+        validate: {
+            validator: (v) => validator.isEmail(v),
+            message: (props) => `${props.value} is not a value field`
+        }
     },
     password: {
         type: String,
         required: [true, "Password is required"],
-        minLength: 8,
+        minlength: 8,
         select: false,
     },
+    isOnline: {
+        type: Boolean,
+        default: false
+    },
+    lastSeen: {type: Date},
+    avatarUrl: {type: String}
 }, {
     timestamps: true,
-})
+});
 
-const User =  mongoose.model(User, userSchema);
+userSchema.index({ username: 1}, { unique: true });
+userSchema.index({ email: 1}, { unique: true });
+
+const User = mongoose.models.User ||  mongoose.model(User, userSchema);
 
 export default User;
