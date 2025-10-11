984880cbd5d6dd5204d34f0d8dc28b1c0d8d5a3a
Varun0856 <Varun0856@users.noreply.github.com>
2025-10-11 22:49:44 +0530

    expose room endpoints on / route on app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import cookieParser from 'cookie-parser';
 import express from 'express'
 import authRouter from './routes/auth.route.js';
+import roomRouter from './routes/room.route.js';
 
 const app = express();
 
@@ -17,11 +18,18 @@ app.get('/', (req, res) => {
             register: "POST /api/v1/auth/register",
             login: "POST /api/v1/auth/login",
             logout: "POST /api/v1/auth/logout",
-            generateRefreshToken: "POST /api/v1/auth/refreshToken"
+            generateRefreshToken: "POST /api/v1/auth/refreshToken",
+            createRoom: "POST /api/v1/room/",
+            joinRoom: 'POST /api/v1/room/:roomId/join',
+            updateRoom: 'PATCH /api/v1/room/:roomId',
+            leaveRoom: 'DELETE /api/v1/room/:roomId/members',
+            getUserRooms: 'GET /api/v1/room',
+            getRoomById: 'GET /api/v1/room/:roomId'
         },
     })
 })
 
 app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/room', roomRouter);
 
 export default app;
