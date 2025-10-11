ff357934219b4b1e426d9f02602f3f3f6fcb39ce
Varun0856 <Varun0856@users.noreply.github.com>
2025-10-11 16:31:54 +0530

    added endpoints details

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import cookieParser from 'cookie-parser';
 import express from 'express'
+import authRouter from './routes/auth.route.js';
 
 const app = express();
 
@@ -7,11 +8,20 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser())
 
+
 app.get('/', (req, res) => {
     res.status(200).json({
         status: 'ok',
         baseURL:'/api',
+        endpoints: {
+            register: "POST /api/v1/auth/register",
+            login: "POST /api/v1/auth/login",
+            logout: "POST /api/v1/auth/logout",
+            generateRefreshToken: "POST /api/v1/auth/refreshToken"
+        },
     })
 })
+
+app.use('/api/v1/auth', authRouter);
 
 export default app;
