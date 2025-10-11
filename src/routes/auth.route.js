2840172fb65ee34419e33ca8bd25aa8bc31adaf0
Varun0856 <Varun0856@users.noreply.github.com>
2025-10-11 16:32:26 +0530

    added auth routes

---
+++ src/routes/auth.route.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/auth.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const authRouter = Router();
+
+authRouter.post('/register', registerUser);
+authRouter.post('/login', loginUser);
+authRouter.post('/logout', verifyJWT, logoutUser);
+authRouter.post('/refreshToken', refreshAccessToken);
+
+export default authRouter;
