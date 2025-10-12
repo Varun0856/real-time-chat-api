d89da16d48136dcccf3a7aac1aca123857b19de1
Varun0856 <Varun0856@users.noreply.github.com>
2025-10-12 17:48:57 +0530

    Update: Add public rooms fetching endpoint

--- src/routes/room.route.js
+++ src/routes/room.route.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { createRoom, getRoomById, getUserRooms, joinRoom, leaveRoom, updateRoom } from "../controllers/room.controller.js";
+import { createRoom, getPublicRooms, getRoomById, getUserRooms, joinRoom, leaveRoom, updateRoom } from "../controllers/room.controller.js";
 const roomRouter = Router();
 
 roomRouter.post('/', verifyJWT, createRoom);
+roomRouter.get('/public', getPublicRooms)
+roomRouter.get('/', verifyJWT, getUserRooms);
 roomRouter.post('/:roomId/join', verifyJWT, joinRoom);
 roomRouter.delete('/:roomId/members', verifyJWT, leaveRoom);
 roomRouter.patch('/:roomId', verifyJWT, updateRoom);
-roomRouter.get('/', verifyJWT, getUserRooms);
-roomRouter.get('/:roomId', getRoomById);
+roomRouter.get('/:roomId', verifyJWT, getRoomById);
 
 export default roomRouter;
