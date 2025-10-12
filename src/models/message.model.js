36643cb3146dc7ea8b5034af33a0c3fe75ac35c9
Varun0856 <Varun0856@users.noreply.github.com>
2025-10-12 19:30:25 +0530

    minor change in naming of attributes

--- src/models/message.model.js
+++ src/models/message.model.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema({
-  chatRoomId: {
+  chatRoom: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Room',
     required: true
