c0f27d6ff577bbb6a2e323dfd34fcef43b10a123
Varun0856 <Varun0856@users.noreply.github.com>
2025-10-12 18:47:39 +0530

    Added message Schema model

---
+++ src/models/message.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+  chatRoomId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Room',
+    required: true
+  },
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  content: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 2000
+  },
+  messageType: {
+    type: String,
+    enum: ['text', 'audio', 'video'],
+    default: 'text',
+  },
+  deliveredAt: {
+    type: Date,
+    default: Date.now
+  },
+  readBy: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+});
+
+const Message = mongoose.models.Message || mongoose.model('Message', messageSchema);
+
+export default Message;
