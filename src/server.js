0ecb342926db75405897ef25383c178d83968a7e
Varun0856 <Varun0856@users.noreply.github.com>
2025-10-14 23:09:56 +0530

    update: restructure of socket.io

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ import connectToDB from "./database/mongodb.js";
 import logger from "./utils/winstonLogger.js";
 import { Server } from "socket.io";
 import http from 'http';
+import { setupSocket } from "./socket/socket.handler.js";
+
 
 process.on('uncaughtException', (error) => {
     logger.error(`UncaughtException: ${error}`)
@@ -14,27 +16,12 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "*"
+        origin: "*",
+        methods: ["GET", "POST"]
     }
 });
 
-io.on('connection', (socket) => {
-    logger.info(`User connected, ${socket.id}`);
-
-    socket.on('join-room', (roomId) => {
-        socket.join(roomId);
-        logger.info(`Socket ${socket.id} joined room ${roomId}`)
-        
-        socket.to(roomId).emit('user-joined', {
-            message: `User ${socket.id} joined the room`
-        })
-    })
-
-
-    socket.on('disconnected', () => {
-        logger.info(`User disconnected ${socket.id}`);
-    });
-});
+setupSocket(io);
 
 const startServer = async () => {
     try {
