dfa05f2b9686110b4eb76492ddd8825593ade172
Varun0856 <Varun0856@users.noreply.github.com>
2025-10-15 22:55:44 +0530

    fix: import added on socket handler file

--- src/socket/socket.handler.js
+++ src/socket/socket.handler.js
@@ -2,6 +2,7 @@ import registerConnectionEvent from "./events/connection.event.js";
 import registerMessageEvents from "./events/message.event.js";
 import registerRoomEvents from "./events/room.event.js";
 import { authMiddleware } from "./middlewares/auth.middleware.js";
+import logger from "../utils/winstonLogger.js";
 
 const setupSocket = (io) => {
     io.use(authMiddleware);
