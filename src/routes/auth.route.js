1e290456290a5ee275c6534af36fc51e8120f9a0
Varun0856 <Varun0856@users.noreply.github.com>
2025-10-16 23:17:21 +0530

    update: use of multer middleware in the route

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/auth.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const authRouter = Router();
 
-authRouter.post('/register', registerUser);
+authRouter.post('/register',upload.single('avatar'),registerUser);
 authRouter.post('/login', loginUser);
 authRouter.post('/logout', verifyJWT, logoutUser);
 authRouter.post('/refreshToken', refreshAccessToken);
