988ee39966f92051ca4b21751edc38b0f5321f65
Varun0856 <Varun0856@users.noreply.github.com>
2025-10-19 11:54:57 +0530

    feat: add online status feature when user connects

--- src/socket/socket.handler.js
+++ src/socket/socket.handler.js
@@ -3,12 +3,33 @@ import registerMessageEvents from "./events/message.event.js";
 import registerRoomEvents from "./events/room.event.js";
 import { authMiddleware } from "./middlewares/auth.middleware.js";
 import logger from "../utils/winstonLogger.js";
+import User from "../models/user.model.js";
+import Room from "../models/room.model.js";
 
 const setupSocket = (io) => {
     io.use(authMiddleware);
 
-    io.on('connection', (socket) => {
+    io.on('connection', async (socket) => {
         logger.info(`User connected, ${socket.user.userId}`);
+        const userId = socket.user.userId;
+        // const user = await User.findById(userId);
+        // if(!user){
+        //     socket.emit('error', {
+        //         message: 'Invalid token'
+        //     });
+        //     return;
+        // }
+        // user.isOnline = true;
+        // await user.save();
+        await User.findByIdAndUpdate(userId, {isOnline: true});
+
+        const rooms = await Room.find({members: userId});
+        rooms.forEach(room => {
+            io.to(room._id.toString()).emit('user-online', {
+                userId,
+                roomId: room._id
+            })
+        })
 
         registerConnectionEvent(io, socket);
         registerRoomEvents(io, socket);
