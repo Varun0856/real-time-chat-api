a770cb5f59b3b6553ab33b977b9846ad69bfbdd4
Varun0856 <Varun0856@users.noreply.github.com>
2025-11-01 14:24:53 +0530

    update: include the global error handler middleware in the app.js

--- src/app.js
+++ src/app.js
@@ -3,34 +3,36 @@ import express from 'express'
 import authRouter from './routes/auth.route.js';
 import roomRouter from './routes/room.route.js';
 import uploadRouter from './routes/upload.route.js';
+import errorHandler from './middlewares/error.middleware.js';
 
 const app = express();
 
 app.use(express.json())
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
+app.use(errorHandler);
 
 
 app.get('/', (req, res) => {
-    res.status(200).json({
-        status: 'ok',
-        baseURL:'/api',
-        endpoints: {
-            register: "POST /api/v1/auth/register",
-            login: "POST /api/v1/auth/login",
-            logout: "POST /api/v1/auth/logout",
-            generateRefreshToken: "POST /api/v1/auth/refreshToken",
-            createRoom: "POST /api/v1/room/",
-            joinRoom: 'POST /api/v1/room/:roomId/join',
-            updateRoom: 'PATCH /api/v1/room/:roomId',
-            leaveRoom: 'DELETE /api/v1/room/:roomId/members',
-            getUserRooms: 'GET /api/v1/room',
-            getRoomById: 'GET /api/v1/room/:roomId',
-            getPublicRooms: 'GET /api/v1/room/public',
-            getMessages: 'GET /api/v1/room/:roomId/messages',
-	        uploadImages: 'POST /api/v1/upload/image'
-        },
-    })
+  res.status(200).json({
+    status: 'ok',
+    baseURL: '/api',
+    endpoints: {
+      register: "POST /api/v1/auth/register",
+      login: "POST /api/v1/auth/login",
+      logout: "POST /api/v1/auth/logout",
+      generateRefreshToken: "POST /api/v1/auth/refreshToken",
+      createRoom: "POST /api/v1/room/",
+      joinRoom: 'POST /api/v1/room/:roomId/join',
+      updateRoom: 'PATCH /api/v1/room/:roomId',
+      leaveRoom: 'DELETE /api/v1/room/:roomId/members',
+      getUserRooms: 'GET /api/v1/room',
+      getRoomById: 'GET /api/v1/room/:roomId',
+      getPublicRooms: 'GET /api/v1/room/public',
+      getMessages: 'GET /api/v1/room/:roomId/messages',
+      uploadImages: 'POST /api/v1/upload/image'
+    },
+  })
 })
 
 app.use('/api/v1/auth', authRouter);
